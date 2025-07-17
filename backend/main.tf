1c25a80a750dcfc5b84d837fbe10806c3d70717e
snepl <neplsaroj4@gmail.com>
2025-07-17 13:46:17 -0400

    files added

---
+++ backend/main.tf
@@ -0,0 +1,26 @@
+# Configure the AWS Provider
+provider "aws" {
+  profile = "saroj-personal"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "demo-terraform-eks-state-bucket-0"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "terraform-eks-state-locks-0"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
